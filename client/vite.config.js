310207c1d3c386aa361aedaf140985072ac5cfd6
Juliana <jsnorek340@gmail.com>
2025-02-25 16:34:03 -0800

    forgot base name change

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/your-repository-name/',
+  base: '/test-deploy-vite-2/',
   plugins: [react()],
 })
